89f787547b6542c03486484f3331a5f68704b105
ElFabrica <arthur.fabricyo@email.com>
2026-04-09 17:24:20 -0300

    stream implemented

---
+++ src/app/api/stream/route.ts
@@ -0,0 +1,25 @@
+import { openai } from "@ai-sdk/openai";
+import { streamText } from "ai";
+
+export async function POST(req: Request) {
+  try {
+    const { prompt } = await req.json();
+    const result = streamText({
+      model: openai("gpt-4.1-nano"),
+      prompt,
+    });
+
+    result.usage.then((usage) => {
+      console.log({
+        inputTokens: usage.inputTokens,
+        outputTokens: usage.outputTokens,
+        totalTokens: usage.totalTokens,
+      });
+    });
+
+    return result.toUIMessageStreamResponse();
+  } catch (error) {
+    console.log("Error generating text: ", error);
+    return new Response("Faied to stram text", { status: 500 });
+  }
+}
